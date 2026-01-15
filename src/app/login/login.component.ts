f6b19e8de74c1e954296e579424e3b855d859f16
Fahad Iqbal <fahadiqbal5049@gmail.com>
2026-01-15 07:44:59 +0500

    updated login functionality and design

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,7 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
-import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -10,34 +10,34 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
   loginForm!: FormGroup;
-  constructor(private formbuilder: FormBuilder, private _http:HttpClient, private _router:Router ) { }
+  returnUrl = '/';
+
+  constructor(
+    private fb: FormBuilder,
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    this.loginForm = this.formbuilder.group({
-      email: [''],
-      password: ['']
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
     });
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
-  logIn() {
-    //console.log(this.loginForm.value);
-    this._http.get<any>('http://localhost:3000/signup').subscribe(
-      (res) => {
-        const user= res.find((a:any)=>{
-          return a.email === this.loginForm.value.email && a.password === this.loginForm.value.password;
-        });
-         if (user) {
-          alert(user.name + ' logged in successfully');
-          this._router.navigate(['/restaurent']);
-          this.loginForm.reset();
-         } else {
-          alert("Invalid credentials");
-         }
-        }, err=>{
-          console.log(err);
-        })
-      }
-    
+  logIn(): void {
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+    const { email, password } = this.loginForm.value;
+    this.auth.login(email, password).subscribe({
+      next: () => this.router.navigateByUrl(this.returnUrl),
+      error: (err) => alert('Login failed: ' + (err?.message || 'Invalid credentials'))
+    });
   }
+}
 
 
